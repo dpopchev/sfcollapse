3995847da21b4c10cf6be66ce74ec744e4d53d4b
Dimitar Popchev <dpopchev@gmail.com>
2023-10-04 10:00:05 +0300

    refactor to test class

--- tests/data_aggregation/test_aggregation_api.py
+++ tests/data_aggregation/test_aggregation_api.py
@@ -5,26 +5,30 @@ import pytest
 
 from sfcollapse.data_aggregation import DataAggregator
 
-PATH_CONTENT = ['out640-01.txt', 'out640-02.txt', 'out640-03.txt', 'out640.txt']
-FILE_PATTERN = re.compile(r'out640-(\d+).txt')
+class TestAggregatorApi:
+    PATH_CONTENT = ['out640-01.txt', 'out640-02.txt', 'out640-03.txt', 'out640.txt']
+    FILE_PATTERN = re.compile(r'out640-(\d+).txt')
 
-@pytest.fixture
-def make_path_stub(mocker):
-    stub = mocker.Mock()
+    @pytest.fixture
+    def make_path_stub(self, mocker):
+        stub = mocker.Mock()
 
-    def factory(iterdir: Iterable[str]):
-        stub.iterdir.return_value = iterdir
-        return stub
+        def factory(iterdir: Iterable[str]):
+            stub.iterdir.return_value = iterdir
+            return stub
 
-    return factory
+        return factory
 
-@pytest.fixture
-def aggregator(make_path_stub):
-    return DataAggregator(make_path_stub(PATH_CONTENT), FILE_PATTERN)
+    @pytest.fixture
+    def aggregator(self, make_path_stub):
+        return DataAggregator(make_path_stub(self.PATH_CONTENT), self.FILE_PATTERN)
 
-def test_aggregator_finds_data_sources_using_pattern(aggregator):
-    assert aggregator.sources == [
-        'out640-01.txt',
-        'out640-02.txt',
-        'out640-03.txt',
-    ]
+    def test_data_sources_are_filtered_in_path(self, aggregator):
+        assert aggregator.sources == [
+            'out640-01.txt',
+            'out640-02.txt',
+            'out640-03.txt',
+        ]
+
+    def test_order_is_extracted_from_source_names(self, aggregator):
+        pass
