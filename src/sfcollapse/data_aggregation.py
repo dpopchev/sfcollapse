24c020740220c2714fafbbb45fa51fe891e6e718
Dimitar Popchev <dpopchev@gmail.com>
2023-10-04 10:44:03 +0300

    test api filtering path

--- src/sfcollapse/data_aggregation.py
+++ src/sfcollapse/data_aggregation.py
@@ -13,8 +13,13 @@ class DataAggregator:
     _pattern: Pattern
 
     @property
-    def sources(self) -> Iterable:
+    def sources(self) -> Iterable[str]:
         return sorted(
-            p for p in self._path.iterdir()
-            if self._pattern.search(p)
+            p.name for p in self._path.iterdir()
+            if self._pattern.search(str(p))
         )
+
+    @property
+    def sequence(self, group=1) -> Iterable[float]:
+        groups = (self._pattern.search(s) for s in self.sources)
+        return [float(g.group(group)) for g in groups] # type: ignore
--- tests/data_aggregation/test_aggregation_api.py
+++ tests/data_aggregation/test_aggregation_api.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import re
 from typing import Iterable
 
@@ -5,23 +6,30 @@ import pytest
 
 from sfcollapse.data_aggregation import DataAggregator
 
+from random import shuffle
+
 class TestAggregatorApi:
+    PATH_PREFIX = 'somedir'
     PATH_CONTENT = ['out640-01.txt', 'out640-02.txt', 'out640-03.txt', 'out640.txt']
     FILE_PATTERN = re.compile(r'out640-(\d+).txt')
+
+    @pytest.fixture(autouse=True)
+    def randomize_path_content_order(self):
+        shuffle(self.PATH_CONTENT)
 
     @pytest.fixture
     def make_path_stub(self, mocker):
         stub = mocker.Mock()
 
-        def factory(iterdir: Iterable[str]):
-            stub.iterdir.return_value = iterdir
+        def factory(path_prefix: str, iterdir: Iterable[str]):
+            stub.iterdir.return_value = [Path(f"{path_prefix}/{d}") for d in iterdir]
             return stub
 
         return factory
 
     @pytest.fixture
     def aggregator(self, make_path_stub):
-        return DataAggregator(make_path_stub(self.PATH_CONTENT), self.FILE_PATTERN)
+        return DataAggregator(make_path_stub(self.PATH_PREFIX, self.PATH_CONTENT), self.FILE_PATTERN)
 
     def test_data_sources_are_filtered_in_path(self, aggregator):
         assert aggregator.sources == [
@@ -30,5 +38,5 @@ class TestAggregatorApi:
             'out640-03.txt',
         ]
 
-    def test_order_is_extracted_from_source_names(self, aggregator):
-        pass
+    def test_sequence_extracted_from_source_names(self, aggregator):
+        assert aggregator.sequence == [1, 2, 3]
