5bb082581657f07dbc223c9e0497a9e98f369584
Dimitar Popchev <dpopchev@gmail.com>
2023-10-04 09:45:52 +0300

    chore: duty message for small self-explanatory changes

--- tests/data_aggregation/test_aggregation_api.py
+++ tests/data_aggregation/test_aggregation_api.py
@@ -5,11 +5,8 @@ import pytest
 
 from sfcollapse.data_aggregation import DataAggregator
 
-
-@pytest.fixture
-def make_aggregator():
-    return DataAggregator
-
+FILE_PATTERN = re.compile(r'out640-(\d+).txt')
+PATH_CONTENT = ['out640-01.txt', 'out640-02.txt', 'out640-03.txt', 'out640.txt']
 
 @pytest.fixture
 def make_path_stub(mocker):
@@ -18,14 +15,21 @@ def make_path_stub(mocker):
     def factory(iterdir: Iterable[str]):
         stub.iterdir.return_value = iterdir
         return stub
+
     return factory
+
+def make_aggregator():
+    return DataAggregator
+
+class TestAggregationProperties:
+
+    @pytest.fixture
+    def aggregator(self):
 
 
 def test_aggregator_finds_data_sources_using_pattern(
         make_aggregator, make_path_stub,
 ):
-    pattern = re.compile(r'out640-(\d+).txt')
-    content = ['out640-01.txt', 'out640-02.txt', 'out640-03.txt', 'out640.txt']
     aggregator = make_aggregator(make_path_stub(content), pattern)
     assert aggregator.sources == [
         'out640-01.txt',
