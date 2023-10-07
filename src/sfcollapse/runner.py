84d77c2df40c03f04aeb20431a468d9738a363ff
Dimitar Popchev <dpopchev@gmail.com>
2023-10-07 12:53:19 +0300

    sandbox runner

--- src/sfcollapse/runner.py
+++ src/sfcollapse/runner.py
@@ -3,13 +3,9 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 import nbformat
-from nbconvert.preprocessors import ExecutePreprocessor
-
-if TYPE_CHECKING:
-    from argparse import ArgumentParser
+from nbconvert.preprocessors.execute import ExecutePreprocessor
 
 
 def copy_into(source: Path, destination: Path) -> None:
@@ -17,10 +13,9 @@ def copy_into(source: Path, destination: Path) -> None:
     destination.joinpath(source.name).write_text(source.read_text())
 
 
-def mark_notebook_executed(nb_path: Path) -> None:
+def mark_notebook_executed(nb_path: Path) -> Path:
     """due to the notebook mechanism preserve the cell output into .executed nb"""
-
-    return nb_path.parent / f"{nb_path.stem}.executed"
+    return nb_path.parent / f"{nb_path.stem}_executed{nb_path.suffix}"
 
 
 def execute_notebook(nb_path: Path) -> None:
@@ -41,43 +36,13 @@ def execute_notebook(nb_path: Path) -> None:
         with open(mark_notebook_executed(nb_path), 'w', encoding='utf-8') as executed_handler:
             nbformat.write(nb_inmemory, executed_handler)
 
+def sandbox_notebook(nb: Path, mark: str = 'sandbox') -> Path:
+    sandboxed_nb = nb.parent.joinpath(f"{mark}_{nb.name}")
+    sandboxed_nb.write_text(nb.read_text())
+    return sandboxed_nb
 
 def sandbox_run(
     notebook: Path,
-    config: Path,
-    destination: Path = Path('build'),
     ) -> None:
     """run notebook in sandbox"""
-    destination.mkdir(parents=True, exist_ok=True)
-    copy_into(notebook, destination)
-    copy_into(config, destination)
-    execute_notebook(destination.joinpath(notebook.name))
-
-
-def hydrate_parser(parser: ArgumentParser):
-    parser.add_argument(
-        '-nb', '--notebook',
-        help='notebook implementation of the collapse simulation',
-        default='dpopchev/scalar_field_collapse.ipynb',
-        type=Path,
-    )
-    parser.add_argument(
-        '-c', '--config',
-        help='notebook configuration',
-        default='dpopchev/scalar_field_collapse.ini',
-        type=Path,
-    )
-    parser.add_argument(
-        '-s', '--sandbox',
-        default='build',
-        help='destination to execute the notebook and store results',
-        type=Path,
-    )
-
-    parser.set_defaults(
-        driver=lambda _: sandbox_run(
-            _.notebook,
-            _.config,
-            _.sandbox,
-        ),
-    )
+    execute_notebook(sandbox_notebook(notebook))
