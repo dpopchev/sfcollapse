c27f012ecea438691b1f7b588d2173cc7e19ca84
Dimitar Popchev <dpopchev@gmail.com>
2023-10-15 11:25:10 +0300

    fix dangeraous default value

--- src/sfcollapse/phenomena.py
+++ src/sfcollapse/phenomena.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from pathlib import Path
 import re
-from typing import Any
+from typing import Any, Dict, Iterable
 from sfcollapse.runner import make_config, sandbox_run, NOTEBOOK_NAME
 from sfcollapse.data_aggregation import make_whitespace_reader, DataAggregator
 from timeit import default_timer as timer
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 NotableAmplitudeRun = namedtuple('NotableRun', 'name amplitude')
 
-NOTABLE_AMPLITUDE_RUNS = [
+NOTABLE_AMPLITUDE_RUNS = (
     NotableAmplitudeRun('default', '0.4'),
     NotableAmplitudeRun('paper', '0.336426615643'),
     NotableAmplitudeRun('miguel', '0.3033260605'),
@@ -19,13 +19,13 @@ NOTABLE_AMPLITUDE_RUNS = [
     NotableAmplitudeRun('dpguess2', '0.28'),
     NotableAmplitudeRun('exploretion-left', '0.30340'),
     NotableAmplitudeRun('exploration-right','0.30359'),
-]
+)
 
-EXPLORE_RUNS = [
+EXPLORE_RUNS = (
     {'name': 'nx0-320', 'amplitude': '0.30340', 'nx0': '320'},
     {'name': 'nx0-640', 'amplitude': '0.30340', 'nx0': '640'},
     {'name': 'nx0-960', 'amplitude': '0.30340', 'nx0': '960'},
-]
+)
 
 DEFAULT_RUNS = EXPLORE_RUNS
 
@@ -42,9 +42,9 @@ def do_run(run, notebook = NOTEBOOK_NAME):
     sandbox_run(Path(notebook), make_config(**run))
     logger.info(f'END {run["name"]}, time ellapsed = {timer() - start:.3f}s')
 
-def run(runs: Any = DEFAULT_RUNS):
+def run(runs: Iterable[Dict] = DEFAULT_RUNS):
     for notable in runs:
         do_run(notable)
 
-def make_run_aggregators(runs: Any = DEFAULT_RUNS):
+def make_run_aggregators(runs: Iterable[Dict] = DEFAULT_RUNS):
     return {r['name']: DataAggregator( make_data_home(r['name']), PATTERN, reader=reader) for r in runs}
